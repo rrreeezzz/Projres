2d8fd6568cb7e8367eadd1d9660709ae632d560c
Lorin <lorinmace@gmail.com>
2016-02-22 08:59:20 +0100

    Ajout BDD.sql
    A lancer dans la console SQL afin de créer la base de donnée

---
+++ BDD.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.10
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost:8889
+-- Généré le :  Lun 22 Février 2016 à 08:43
+-- Version du serveur :  5.5.38
+-- Version de PHP :  5.6.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `files`
+--
+
+CREATE TABLE `files` (
+`Id` int(11) NOT NULL,
+  `Owner` int(11) NOT NULL,
+  `Receiver` varchar(255) NOT NULL,
+  `Send_Date` date NOT NULL,
+  `Nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groups`
+--
+
+CREATE TABLE `groups` (
+`Id` int(11) NOT NULL,
+  `Name` varchar(15) NOT NULL,
+  `Owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+`Id` int(11) NOT NULL,
+  `Name` varchar(15) NOT NULL,
+  `Creation` date NOT NULL,
+  `Last_Connection` date NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `files`
+--
+ALTER TABLE `files`
+ ADD PRIMARY KEY (`Id`), ADD KEY `Owner` (`Owner`);
+
+--
+-- Index pour la table `groups`
+--
+ALTER TABLE `groups`
+ ADD PRIMARY KEY (`Id`), ADD KEY `Owner` (`Owner`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `files`
+--
+ALTER TABLE `files`
+MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `groups`
+--
+ALTER TABLE `groups`
+MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `files`
+--
+ALTER TABLE `files`
+ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`Owner`) REFERENCES `users` (`ID`);
+
+--
+-- Contraintes pour la table `groups`
+--
+ALTER TABLE `groups`
+ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`Owner`) REFERENCES `users` (`ID`);
