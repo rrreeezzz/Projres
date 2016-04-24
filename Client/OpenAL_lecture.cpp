cddfc1230bffd19cc4fed9628f07b10a7d89d2fa
EzLucky <EzLucky@users.noreply.github.com>
2016-04-25 00:40:29 +0200

    KKtus too noob

--- Client/OpenAL_lecture.cpp
+++ Client/OpenAL_lecture.cpp
@@ -10,8 +10,8 @@
   #include <OpenAL/al.h>
   #include <OpenAL/alc.h>
 #else
-  #include <al/al.h>
-  #include <al/alc.h>
+  #include <AL/al.h>
+  #include <AL/alc.h>
 #endif
 #include <sndfile.h>
 #include <iomanip>
